01d594708d51b6e66695f77574b2f803a136bbd9
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2023-02-27 22:06:17 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- print_r($_POST);
+$comment = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["comment"])) {
+ $comment = $_POST["comment"];
 }
+echo "Comment: $comment";
